492609c4c8db6f061d2ee9c648b08b7252f40637
Lee-1129 <65951989+Lee-1129@users.noreply.github.com>
2021-07-19 10:27:49 +0800

    新增视频显示demo

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # coding:utf-8
-from importlib import import_module
 
-from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify, send_file, Response
+from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify, send_file
 from werkzeug.utils import secure_filename
 import os
 import cv2
@@ -29,8 +28,9 @@ app.send_file_max_age_default = timedelta(seconds=1)
 @app.route('/upload', methods=['POST', 'GET'])  # 添加路由
 def upload():
     if request.method == 'POST':
-        shutil.rmtree("static/images")
-        os.makedirs("static/images")
+        basepath = os.path.dirname(__file__)  # 当前文件所在路径
+        shutil.rmtree(os.path.join(basepath, 'static', 'images'))
+        os.makedirs(os.path.join(basepath, 'static', 'images'))
         f = request.files['file']
 
         if not (f and allowed_file(f.filename)):
@@ -39,7 +39,7 @@ def upload():
 
         user_input = request.form.get("name")
 
-        basepath = os.path.dirname(__file__)  # 当前文件所在路径
+
 
         upload_path = os.path.join(basepath, 'static/images', secure_filename(f.filename))  # 注意：没有的文件夹一定要先创建，不然会提示没有该路径
         # upload_path = os.path.join(basepath, 'static/images','test.jpg')  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
@@ -66,31 +66,25 @@ def show():
         然后将生成的图片保存到本地，最后再将识别结果传到前端进行展示
     '''
     '''结果存放于/static/results下'''
-    # filename = 'static/results/1.jpg'
-    # request_begin_time = datetime.today()
-    # print("request_begin_time", request_begin_time)
-    # if request.method == 'GET':
-    #     if filename is None:
-    #         print('None result!')
-    #     else:
-    #         return send_file(filename)
-    # else:
-    #     pass
-    # return "error"
-    # 这里修改一下对接的路径即可
-    img_path = 'd:project/static/results/1.jpg'
+    # 使用获取文件的路径并显示文件
+    basepath = os.path.dirname(__file__)  # 当前文件所在路径
+    # 下面两行为results文件夹的清除与建立（清理缓存功能），与识别模型结合再取消注释
+    # shutil.rmtree(os.path.join(basepath, 'static', 'images'))
+    # os.makedirs(os.path.join(basepath, 'static', 'images'))
+    img_path = os.path.join(basepath, 'static/results', 'test.jpg')
     img_stream = showimg(img_path)
-    return render_template('imgshow.html', data=img_stream)
+    return render_template('imgshow.html', image=img_stream)
 
 
 def showimg(filename):
+
     img_stream = ''
     with open(filename, 'rb') as img:
+
         img_stream = img.read()
         #  print(base64.b16encode(img_stream))
         img_stream = base64.b64encode(img_stream).decode()
     return img_stream
-
 
 '''视频播放'''
 @app.route('/vshow')
@@ -110,5 +104,5 @@ def movie_list():
 
 if __name__ == '__main__':
     app.run(debug=True, port=81)
-    # show()
-    # test()
+    #show()
+    #test()
